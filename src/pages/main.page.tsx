3fec3f74c12e17500db7e62af5c390536d888b5a
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-08-07 21:43:06 +0900

    fix

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ButtonType } from '../common/constants/enum'
 import Button from '../components/elements/button'
 import Input from '../components/elements/input'
+import Confirm from '../components/modals/confirm.modal'
 
 const MainPage = () => {
   const [value, setValue] = useState('')
+
+  const [modal, setModal] = useState(false)
+
   const click = (e: any) => {
-    console.log(value)
+    console.log(123123)
   }
   const change = (e: any) => {
     setValue(e.target.value)
   }
+
   return (
-    <div>
+    <div className="w-screen h-screen flex">
       <div>Main Page</div>
       <div className="flex flex-row justify-start">
         <Input
@@ -21,10 +26,19 @@ const MainPage = () => {
           placeholder="입력하시라"
           onChange={change}
         />
-        <Button className="mt-2 ml-2" onClick={click} type={ButtonType.success}>
+        <Button
+          className="mt-2 ml-2"
+          click={() => setModal(true)}
+          type={ButtonType.success}
+        >
           수정
         </Button>
       </div>
+      <Confirm
+        visibleSync={modal}
+        message="모달온"
+        close={() => setModal(false)}
+      />
     </div>
   )
 }
