c158fc969cf1c177614f0968475aa321d6e0da70
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-08-27 15:54:33 +0900

    fix navigator

--- src/components/navigator.component.tsx
+++ src/components/navigator.component.tsx
@@ -1,11 +1,36 @@
-import { AppBar, Box } from '@mui/material'
+import { AccountCircle } from '@mui/icons-material'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
 
 const Navigator = () => {
   return (
-    <Box>
-      <AppBar position="static">hi</AppBar>
-    </Box>
+    <AppBar position="fixed" color="inherit">
+      <Box>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: 'monospace',
+                color: 'inherit',
+                letterSpacing: '.3rem',
+                textDecoration: 'none',
+                fontWeight: 700
+              }}
+              noWrap
+              component="a"
+              href="/"
+            >
+              HomePage
+            </Typography>
+          </Box>
+          <IconButton size="large" color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </Box>
+    </AppBar>
   )
 }
 
