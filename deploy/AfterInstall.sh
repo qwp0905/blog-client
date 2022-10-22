806c672f607d33a6bfa37b5b1f7845f6ea0508d3
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-10-22 13:58:17 +0900

    배포 방식 변경

--- deploy/AfterInstall.sh
+++ deploy/AfterInstall.sh
@@ -10,14 +10,14 @@ sudo apt install -y docker.io
 
 cd ${BASE_PATH}
 
+sudo docker pull qwp1216/blog-client
+
 sudo docker rm -f web-client
-sudo docker build -f Dockerfile.prod -t web-client .
+# sudo docker build -f Dockerfile.prod -t web-client .
 sudo docker run -d \
   -p 80:80 \
   -p 443:443 \
   --name web-client \
-  -v ${BASE_PATH}/key.pem:/etc/key.pem \
-  -v ${BASE_PATH}/cert.pem:/etc/cert.pem \
   web-client
 
 sudo docker images --quiet --filter=dangling=true | sudo xargs --no-run-if-empty docker rmi
